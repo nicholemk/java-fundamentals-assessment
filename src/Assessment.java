86b7c16c897c6fb3c6042ea398c72ef3b8d03e91
nicholekirlin <nichole.kirlin@gmail.com>
2017-12-14 14:20:22 -0600

    assessment q 1 done

--- src/Assessment.java
+++ src/Assessment.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class Assessment {
     public static void main(String[] args) {
-        System.out.println("hey");
+        Scanner sc=new Scanner(System.in);
+        int num;
+
+        System.out.print("Enter one integer: ");
+        num=sc.nextInt();
+
+        System.out.println("Square of "+ num + " is: "+ Math.pow(num, 2));
+
+        int x, y, z;
+        System.out.println("Enter two integers: ");
+        Scanner in = new Scanner(System.in);
+        x = in.nextInt();
+        y = in.nextInt();
+        z = x + y;
+        System.out.println("Sum = "+z);
+
+
+        double[] vals = new double[5];
+        System.out.print("Please enter five integers: ");
+
+        for (int i = 0; i < 5; i++)
+            vals[i] = in.nextDouble();
+
+        System.out.printf("The average is: %.2f", average(vals));
     }
+
+    public static int average(int[] array) {
+        int sum = 0;
+        for (int val : array)
+            sum += val;
+        return sum / array.length;
+    }
+
+    public static double average(double[] array) {
+        double sum = 0.0;
+        for (double val : array)
+            sum += val;
+        return sum / array.length;
+    }
+
+
 }
+
+
